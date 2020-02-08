d105de3d26d39e215863cb469601c4d5ad6243ba
Matěj Červenka <matej.cervenka03@gmail.com>
2020-02-08 12:23:05 +0100

    :D

--- rpi_1/dht_fb.py
+++ rpi_1/dht_fb.py
@@ -1,4 +1,4 @@
-# import Adafruit_DHT
+import Adafruit_DHT
 import matplotlib as plt
 import time
 from fbchat import Client
@@ -10,18 +10,23 @@ def get_temp(pin):
     return [hum, temp]
 
 def send_message(message):
-    client = Client('python.analyssis@gmail.com', 'Pythonisbest')
+    client = Client('example@mail.com', 'Password')
     client.send(Message(message), thread_type=ThreadType.USER, thread_id='users id')
     client.logout()
 
 if '__main__' == __name__:
-    # while True:
-    #     if time.localtime().tm_min = 0:
-    #         pass
-    print(time.localtime().tm_min)
-            
+    while True:
+        hum_list = []
+        temp_list = []
+        hour_list = []
+        if time.localtime().tm_min == 0:
+            hum_list.append(get_temp(4)[0])
+            temp_list.append(get_temp(4)[1])
+            hour_list.append(time.localtime().tm_hour)
 
-
-
-
-
+        if time.localtime().tm_hour == 17 and time.localtime().tm_min == 0:
+            send_message(plt.plot(temp_list, hour_list))
+            send_message(plt.plot(hum_list, hour_list))
+            hum_list.clear()
+            temp_list.clear()
+            hour_list.clear()
